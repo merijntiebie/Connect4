f7ec368f7f2a7049be5e8ff57d360a39c6b6ac1d
merijn.tiebie@valcon.com <merijn.tiebie@valcon.com>
2023-06-14 11:17:29 +0200

    chore: again

--- src/template.ts
+++ src/template.ts
@@ -1,6 +1,9 @@
-function templateFunction(x: number) {
-  return x / 2;
+function getPlayerColor(playerNumber: 1 | 2) {
+  if (playerNumber === 1) {
+    return "red";
+  }
+  return "yellow";
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export { templateFunction };
+export { getPlayerColor };
--- test/template.test.ts
+++ test/template.test.ts
@@ -1,7 +1,12 @@
-import { templateFunction } from "../src/template";
+import { getPlayerColor } from "../src/template";
 
-describe("TemplateFunction should return half of the provided", () => {
-  it("2 -> 1 ", () => {
-    expect(templateFunction(2)).toEqual(1);
+describe("This suite tests the game of connect 4. It is a game in which 2 players take turns to drop their discs in a grid. The objective of the game is to be the first to form a horizontal, vertical or diagonal line of four of one's own disc", () => {
+  describe("We have 2 players which play with their own color", () => {
+    it("Player 1 plays with the red discs", () => {
+      expect(getPlayerColor(1)).toBe("red");
+    });
+    it("Player 2 plays with the yellow discs", () => {
+      expect(getPlayerColor(2)).toBe("yellow");
+    });
   });
 });
